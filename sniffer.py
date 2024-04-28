ba9ab7ac83a922c57b50f0799084873252a7d156
Pujan Sarkar <144250917+Pujan-sarkar@users.noreply.github.com>
2024-04-29 01:48:32 +0530

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,13 +1,27 @@
 from scapy.all import *
+from datetime import datetime
 
 interface = 'wlan0'
-probeReqs = []
+probeReqs = {}
 
 def sniffProbes(p):
     if p.haslayer(Dot11ProbeReq):
-        netName = p.getlayer(Dot11ProbeReq).info.decode()
-        if netName not in probeReqs:
-            probeReqs.append(netName)
-            print('[+] Detected Probe Requests: ' + netName)
+        ssid = p.getlayer(Dot11ProbeReq).info.decode()
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        if ssid not in probeReqs:
+            probeReqs[ssid] = 1
+            log_probe(timestamp, ssid)
+            print(f'[+] Detected Probe Request: {ssid}')
+        else:
+            probeReqs[ssid] += 1
 
-sniff(iface=interface, prn=sniffProbes)
+def log_probe(timestamp, ssid):
+    with open('probe_requests.log', 'a') as log_file:
+        log_file.write(f'{timestamp} - {ssid}\n')
+
+def start_sniffing(interface):
+    print(f'Starting sniffing on interface {interface}...')
+    sniff(iface=interface, prn=sniffProbes)
+
+if __name__ == "__main__":
+    start_sniffing(interface)
